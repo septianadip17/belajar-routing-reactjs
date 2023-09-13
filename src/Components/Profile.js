a2390c6af2d37a44f30d53860094e77fcbb66493
Septian Adi Pratama <septianadip7998@gmail.com>
2023-09-14 00:21:33 +0700

    first commit

---
+++ src/Components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+export const Profile = () => {
+  return (
+    <div>
+    <h1>Asik Berhasil</h1>
+    <Link to="/home">Kembali</Link>
+    </div>
+  )
+}
